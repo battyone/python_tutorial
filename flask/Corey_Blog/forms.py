c5f92df38e33531302c4c7a94d533a7bc77fd88d
Christian Henning <chhenning@gmail.com>
2019-01-29 09:22:24 -0500

    Flask Tutorial 3 Part 1

---
+++ flask/Corey_Blog/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+        validators=[DataRequired(), Length(min=2, max=20)])
+    
+    email = StringField('Email', 
+        validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', 
+        validators=[DataRequired()])
+    
+    confirm_password = PasswordField('', 
+        validators=[DataRequired()])
