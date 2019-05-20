fb808778f120d60e09558a1009ae0d93d996a7d7
Christian Henning <chhenning@gmail.com>
2019-05-19 23:00:13 -0400

    More snippets

--- snippets/src/vector.py
+++ snippets/src/vector.py
@@ -1,9 +1,13 @@
+
 # %%
 from math import hypot
 from array import array
 
+# to make Vector hashable it has to immutable (see __x, etc.)
+
+
 class Vector:
-    typecode = 'd'
+    typecode = 'd'  # double
 
     @classmethod
     def frombytes(cls, octets):
@@ -12,11 +16,26 @@ class Vector:
         return cls(*memv)
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __hash__(self):
+        print('hashing')
+        return hash(self.x) ^ hash(self.y)
 
     def __abs__(self):
         return hypot(self.x, self.y)
@@ -40,19 +59,48 @@ class Vector:
     def __str__(self):
         return f'x is {self.x} and y is {self.y}'
 
+    def __format__(self, fmt_spec=''):
+        components = (format(c, fmt_spec) for c in self)
+        return '({} {})'.format(*components)
 
     def __bytes__(self):
         return (bytes([ord(self.typecode)]) +
-        bytes(array(self.typecode, self)))
+                bytes(array(self.typecode, self)))
 
 
 # %%
-v=Vector(23, 89)
+v = Vector(23, 89)
 print(v)
 print(repr(v))
 
 for e in v:
     print(e)
 
-octets=bytes(v)
+octets = bytes(v)
 print(octets)
+
+format(v)
+
+a = Vector(3, 89)
+b = Vector(3, 89)
+print(a == b)
+
+a.__x = 99
+print(str(a))
+print(a.__x)
+print(a.x)
+
+print(hash(a))
+print(hash(b))
+
+#%%
+a = Vector(1.3,2.99)
+print(hash(a))
+print(hash(float(33.1)))
+
+octets = bytes(a)
+print(octets)
+b = Vector.frombytes(octets)
+print(a)
+print(b)
+
